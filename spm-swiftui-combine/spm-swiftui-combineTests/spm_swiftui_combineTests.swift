be6788b12f1378ef14ca6b9fc4ded782076efac3
Tomas Holicky <holickytomas@gmail.com>
2020-05-13 12:30:34 +0200

    Added basic stub

---
+++ spm-swiftui-combine/spm-swiftui-combineTests/spm_swiftui_combineTests.swift
@@ -0,0 +1,34 @@
+//
+//  spm_swiftui_combineTests.swift
+//  spm-swiftui-combineTests
+//
+//  Created by Tomas Holicky on 13/05/2020.
+//  Copyright © 2020 Tomas Holicky. All rights reserved.
+//
+
+import XCTest
+@testable import spm_swiftui_combine
+
+class spm_swiftui_combineTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
